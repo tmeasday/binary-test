ec3c3ce0138e4774151eda31b9ffb09e154f635f
Tom Coleman <tom@thesnail.org>
2016-03-23 14:44:14 +1100

    Testing

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing forcing non-portability of packages
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'binary-test',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Testing forcing non-portability of packages',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/tmeasday/binary-test',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3-rc.6');
+  api.use('ecmascript');
+  api.mainModule('binary-test.js');
+});
