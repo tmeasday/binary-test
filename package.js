241c9daaff1cf34cc63d6ae3dbe3fdf517577f05
Tom Coleman <tom@thesnail.org>
2016-03-23 14:57:09 +1100

    Now using meteor-force-non-portable

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tmeasday:binary-test',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Testing forcing non-portability of packages',
   // URL to the Git repository containing the source code for this package.
@@ -8,6 +8,10 @@ Package.describe({
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
+});
+
+Npm.depends({
+  'meteor-force-non-portable': '1.0.0'
 });
 
 Package.onUse(function(api) {
